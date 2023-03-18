4f40424da43a19fd34f368ecc6713294a4df3c73
StevenMM23 <esteven123123@outlook.com>
2023-03-18 15:45:55 -0400

    Add Lexer Parser

--- ProyectoConstruccion/Properties/AssemblyInfo.cs
+++ ProyectoConstruccion/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProyectoDeConstruccion")]
+[assembly: AssemblyTitle("ProyectoConstruccion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProyectoDeConstruccion")]
+[assembly: AssemblyProduct("ProyectoConstruccion")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0b92422f-85c2-4ba4-95b5-1c59aaa63883")]
+[assembly: Guid("fcd22b7e-905a-4d40-b918-8d7473b8d44d")]
 
 // Version information for an assembly consists of the following four values:
 //
