c133040fa45bf5a08258ad45b8b5d2afd6ff86e6
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-03-23 10:09:06 -0400

    Migrated to .Net 7

--- ProgrammingLanguageANTLR4/Program.cs
+++ ProgrammingLanguageANTLR4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Antlr4.Runtime;
+using ProgrammingLanguageANTLR4;
+
+var project = File.ReadAllText($"C:\\Users\\estev\\Desktop\\ProyectoLanguage\\ProgrammingLanguageANTLR4\\test.txt");
+
+var inputStream = CharStreams.fromString(project);
+var lexer = new SSJLexer(inputStream);
+var tokenStream = new CommonTokenStream(lexer);
+var parser = new SSJParser(tokenStream);
+
+var tree = parser.program();
+var visitor = new Visitor();
+
+var translation = visitor.Visit(tree);
+Console.WriteLine(translation);
+
+Console.ReadKey(true);
